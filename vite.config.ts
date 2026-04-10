98e8f3c463fa7e403215f8f0a7e628b4b41d785b
AshrafQandeel <aqandeel@gmail.com>
2026-04-10 22:14:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
   server: {
     port: 3000,
     host: '0.0.0.0',
   },
+  build: {
+    outDir: 'dist',
+  }
 });
